1405ef234c8f9f3eda0a5847028c829ebf54774f
Thamaraiselvan Kodhandabani <thko2162@stu.sjcc.edu>
2026-02-05 23:46:41 -0800

    added a few extra info

--- assignment1/src/main/java/com/homework/App.java
+++ assignment1/src/main/java/com/homework/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World, Excited to learn a lot about Data Structures!" );
+        System.out.println( "I am also taking the Intro to Web Dev. So, excited to combine all the things that I learn");
     }
 }
